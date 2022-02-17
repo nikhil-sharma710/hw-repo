c40613fdcc52eeecfe5ad289103a0992ed358f28
nikhil-sharma710 <98418670+nikhil-sharma710@users.noreply.github.com>
2022-02-16 21:57:59 -0600

    Update analyze_water.py

--- homework03/analyze_water.py
+++ homework03/analyze_water.py
@@ -4,19 +4,57 @@ import math
 
 logging.basicConfig(level = logging.INFO, format = '%(levelname)s: %(message)s')
 
-def calculate_turbidity(a0: float, I90: float) -> float:
+def create_new_list(turbidity_data: list) -> list:
     """
-    This function calculates the turbidity of water, using the equation T = a0*I90.
-
+    This function takes the list, turbidity_data, and creates a new list of the five most recent dictionaries.
+    
     Args:
-        a0 (float): The first is the calibration constant.
-        I90 (float): The second argument is the 90 degree detector current.
-
+        turbidity_data (list): This list is a list of dictionaries of hourly data about turbidity of water.
+        
     Returns:
-        T (float): The function returns the turbidity of water.
+        most_recent (list): The function returns a list of five dictionaries, being the five most recent from the data.
+    """
+    
+    most_recent = []
+    
+    for i in range(5):
+        most_recent.append(turbidity_data['turbidity_data'][len(turbidity_data) - (6 - i)])
+        
+    return most_recent
+
+def average_values(most_recent: list) -> float:
+    """
+    This function returns the average turbidity of the water based on the five most recent recordings in the data.
+    
+    Args:
+        most_recent (list): This list is a list of the five most recent dictionaries from the data.
+        
+    Returns:
+        average_turbidity (float): The function returns a float of the average turbidity of the five most recent recordings.
+    """
+    
+    total = 0
+
+    for i in range(5):
+        total += calculate_turbidity(most_recent[i], 'calibration_constant', 'detector_current')
+        
+    average_turbidity = total/5
+    return average_turbidity
+
+def calculate_turbidity(dictionary: dict, a0: str, I90: str) -> float:
+    """
+    This function calculates the turbidity of the water, using the formula T = a0*I90.
+    
+    Args:
+        dictionary (dict): The first argument is a specific dictionary whose calibration constant and detector current are multiplied.
+        a0 (str): The second argument is a key string to find the calibration constant.
+        I90 (str): The third argument is a key string to find the 90 degree detector current.
+        
+    Returns:
+        T (float): The function returns the turbidity of the water.
     """
 
-    T = a0*I90
+    T = dictionary[a0]*dictionary[I90]
     return T
 
 def calculate_minimum_time(Ts: float, T0: float, d: float) -> float:
@@ -38,17 +76,8 @@ def calculate_minimum_time(Ts: float, T0: float, d: float) -> float:
 def main():
     with open('turbidity_data.json', 'r') as f:
         turbidity_data = json.load(f)
-
-    most_recent = []
-    turbidity_values = []
-    total = 0
-
-    for i in range(5):
-        most_recent.append(turbidity_data['turbidity_data'][len(turbidity_data) - (6 - i)])
-        total += calculate_turbidity(most_recent[i]['calibration_constant'], most_recent[i]['detector_current'])
-
-    average_turbidity = total/5
-    print('Average turbidity based on most recent five measurements = ' + str(average_turbidity) + ' NTU')
+    
+    print('Average turbidity based on most recent five measurements = ' + average_values(most_recent) + ' NTU')
 
     threshold = 1.0
     decay_factor = 0.02
