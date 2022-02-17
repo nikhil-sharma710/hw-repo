721e7344781e065b811e3e85f90bd1c0415c4f94
nikhil-sharma710 <98418670+nikhil-sharma710@users.noreply.github.com>
2022-02-16 22:45:04 -0600

    Update test_analyze_water.py

--- homework03/test_analyze_water.py
+++ homework03/test_analyze_water.py
@@ -4,17 +4,25 @@ from analyze_water import create_new_list
 from analyze_water import calculate_turbidity
 from analyze_water import calculate_minimum_time
 
-data_1 = {'a': 2.57, 'b': 4.62}
-assert(calculate_turbidity(data_1, 'a', 'b') == 11.8734)
+def test_calculate_turbidity():
+    data_1 = {'a': 2.57, 'b': 4.62}
+    
+    assert(calculate_turbidity(data_1, 'a', 'b') == 11.8734)
+    assert(isinstance(calculate_turbidity(data, 'a', 'b'), float) == True)
 
-a = 0.75
-b = 1.5
-c = 0.5
-assert(calculate_minimum_time(a, b, c) == 1)
-
-data_2 = [1, 2, 3, 4, 5, 6, 7, 8]
-assert(isinstance(create_new_list(data_2), list) == True)
-
-assert(isinstance(calculate_turbidity(data, 'a', 'b'), float) == True)
-
-
+def test_calculate_minimum_time():
+    a = 0.75
+    b = 1.5
+    c = 0.5
+    
+    assert(calculate_minimum_time(a, b, c) == 1)
+    
+    with pytest.raises(TypeError):
+        calculate_minimum_time('a', b, c)
+        
+ def main():
+     test_calculate_turbidity()
+     test_calculate_minimum_time()
+ 
+if __name__ == '__main__':
+    main()
