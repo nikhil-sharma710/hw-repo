03e89991aae9855791bc42477f3391fda1f811af
Nikhil Sharma <nikhilsharma@utexas.edu>
2022-02-16 23:06:00 -0600

    final commit

--- homework03/test_analyze_water.py
+++ homework03/test_analyze_water.py
@@ -5,22 +5,25 @@ from analyze_water import calculate_turbidity
 from analyze_water import calculate_minimum_time
 
 def test_calculate_turbidity():
-    data_1 = {'a': 2.57, 'b': 4.62}
+    data = {'a': 2.57, 'b': 4.62}
     
-    assert(calculate_turbidity(data_1, 'a', 'b') == 11.8734)
+    assert(calculate_turbidity(data, 'a', 'b') == 11.8734)
     assert(isinstance(calculate_turbidity(data, 'a', 'b'), float) == True)
+
+    with pytest.raises(
 
 def test_calculate_minimum_time():
     a = 0.75
     b = 1.5
     c = 0.5
     
-    assert(calculate_minimum_time(a, b, c) == 1)
-    
+    assert(calculate_minimum_time(a, b, c) == 1.0)
+    assert(isinstance(calculate_minimum_time(a, b, c), float) == True)    
+
     with pytest.raises(TypeError):
         calculate_minimum_time('a', b, c)
-        
- def main():
+ 
+def main():
      test_calculate_turbidity()
      test_calculate_minimum_time()
  
